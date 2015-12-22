3b471854cfab88a4c5e1b77deeaf6a58ab10e038
Kazuhiro Kosaka <kosaka_kazuhiro@cyberagent.co.jp>
2015-12-22 12:31:52 +0900

    Added install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install brew-cask
+brew update
+
+brew cask install alfred skype kindle sizeup google-drive google-japanese-ime google-chrome firefox dropbox sublime-text istat-menus line coderunner tunnelbear hipchat slack disk-sensei 1password adobe-creative-cloud virtualbox docker-machine vagrant vagrant-manager mysqlworkbench sourcetree intellij-idea bitcoin-core xamarin-studio unity
+
+brew install vim gmp nodebrew scala sbt mongodb mysql56 stoken git git-flow maven wget redis28 protobuf docker docker-compose svn
+
+# install manually gvm secureid displaymenu canary redis-desktop
+# install via appstore xcode numbers pages keynote omnigraph patterns
